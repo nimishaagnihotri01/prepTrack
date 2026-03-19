d8b966fd486e18e69dfa5c8fa6e4d19b9f8362ed
nimisha <agnihotrinimisha296@gmail.com.com>
2026-03-19 19:19:12 +0530

    deployement issue

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use("/api/learning", learningRoutes); // ⭐ ADD THIS
 app.use("/api/ai", aiRoutes);
 app.use("/api/code", codeRoutes);
 app.use("/api/workspace", workspaceRoutes);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
