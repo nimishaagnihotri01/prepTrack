daba90668eeace66cc142f42b2bd79d4f2656e6f
nimisha <agnihotrinimisha296@gmail.com.com>
2026-03-20 18:09:43 +0530

    fallback axios fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://preptrack-6bxk.onrender.com/api",
 });
 
 export default API;
