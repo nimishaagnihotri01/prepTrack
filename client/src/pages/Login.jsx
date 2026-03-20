5773a0813c0955bb6ae64bf93ce0257a144d8739
nimisha <agnihotrinimisha296@gmail.com.com>
2026-03-20 19:08:14 +0530

    fixed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await API.post("/auth/login", { email, password });
+      const res = await API.post("/api/auth/login", { email, password });
 
       localStorage.setItem("token", res.data.token);
 
